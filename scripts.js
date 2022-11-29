4851ad7bcf7f4976e944341d66bc08cc419ead04
Cesaum <97255173+oCesaum@users.noreply.github.com>
2022-11-29 18:09:19 -0300

    commit 10000

--- scripts.js
+++ scripts.js
@@ -1,41 +1,19 @@
 function changeSelected(e) {
   if (e.target.alt === "Sofá Azul" || e.target.id === "Sofá Azul") {
-    // animações
-    if(document.querySelector('#divider').classList.value.includes("red")) {
-      //de vermelho para azul
-      document.querySelector('#left-container').removeAttribute("class")
-      document.querySelector('#left-container').classList.add("left-container", "rotate-container");
-
-      document.querySelector('#main-bg').removeAttribute("class")
-      document.querySelector('#main-bg').classList.add("main-bg", "red-to-blue");
-
-      setTimeout(() => {  document.querySelector('#left-container').classList.remove("rotate-container"); }, 1501);
-    }
-    if(document.querySelector('#divider').classList.value.includes("yellow")) {
-      //de amarelo para azul
-      document.querySelector('#left-container').removeAttribute("class")
-      document.querySelector('#left-container').classList.add("left-container", "rotate-container");
-
-      document.querySelector('#main-bg').removeAttribute("class")
-      document.querySelector('#main-bg').classList.add("main-bg", "yellow-to-blue");
-
-      setTimeout(() => {  document.querySelector('#left-container').classList.remove("rotate-container"); }, 1501);
-    }
-
     //troca o sofá principal para azul
-    setTimeout(() => {
-      document.querySelector('#couch-img').src = "images/001-sofa.png";
-      document.querySelector('#couch-img').alt = "Sofá azul";
-      document.querySelector('#couch-img').title = "Sofá azul";
+    document.querySelector('#left-object-rotable-container').removeAttribute("class")
+    document.querySelector('#left-object-rotable-container').classList.add("left-object-rotable-container", "blur-container");
 
-      document.querySelector('#couch-img-2').src = "images/002-sofa-1.png";
-      document.querySelector('#couch-img-2').alt = "Sofá amarelo";
-      document.querySelector('#couch-img-2').title = "Sofá amarelo";
+    document.querySelector('#couch-rotable-1').removeAttribute("class")
+    document.querySelector('#couch-rotable-1').classList.add("couch-rotable-1", "rotate-0");
 
-      // document.querySelector('#couch-img-3').src = "images/003-sofa-2.png";
-      // document.querySelector('#couch-img-3').alt = "Sofá vermelho";
-      // document.querySelector('#couch-img-3').title = "Sofá vermelho";
-    }, 500);
+    document.querySelector('#couch-rotable-2').classList.remove("rotate-0")
+    document.querySelector('#couch-rotable-2').classList.add("rotate-175")
+    document.querySelector('#couch-rotable-3').classList.remove("rotate-0")
+    document.querySelector('#couch-rotable-3').classList.add("rotate-175")
+
+    document.querySelector('#main-bg').removeAttribute("class")
+    document.querySelector('#main-bg').classList.add("main-bg", "rotate-15");
 
     //troca os 3 sofás para azul
     document.querySelector('#couch-1').src = "images/001-sofa.png";
@@ -50,44 +28,26 @@ function changeSelected(e) {
     document.querySelector('.blue-couch').classList.add("active");
     document.querySelector('.yellow-couch').classList.remove("active");
     document.querySelector('.red-couch').classList.remove("active");
+
+    setTimeout(() => {  document.querySelector('#left-object-rotable-container').classList.remove("blur-container"); }, 1500);
   }
   if (e.target.alt === "Sofá Amarelo" || e.target.id === "Sofá Amarelo") {
-    // animações
-    if(document.querySelector('#divider').classList.value.includes("blue")) {
-      //de azul para amarelo
-      document.querySelector('#left-container').removeAttribute("class")
-      document.querySelector('#left-container').classList.add("left-container", "rotate-container");
-
-      document.querySelector('#main-bg').removeAttribute("class")
-      document.querySelector('#main-bg').classList.add("main-bg", "blue-to-yellow");
-
-      setTimeout(() => {  document.querySelector('#left-container').classList.remove("rotate-container"); }, 1501);
-    }
-    if(document.querySelector('#divider').classList.value.includes("red")) {
-      //de vermelho para amarelo
-      document.querySelector('#left-container').removeAttribute("class")
-      document.querySelector('#left-container').classList.add("left-container", "rotate-container");
-
-      document.querySelector('#main-bg').removeAttribute("class")
-      document.querySelector('#main-bg').classList.add("main-bg", "red-to-yellow");
-
-      setTimeout(() => {  document.querySelector('#left-container').classList.remove("rotate-container"); }, 1501);
-    }
-
     //troca o sofá principal para amarelo
-    setTimeout(() => {
-      document.querySelector('#couch-img').src = "images/002-sofa-1.png";
-      document.querySelector('#couch-img').alt = "Sofá amarelo";
-      document.querySelector('#couch-img').title = "Sofá amarelo";
+    document.querySelector('#left-object-rotable-container').removeAttribute("class")
+    document.querySelector('#left-object-rotable-container').classList.add("left-object-rotable-container", "blur-container");
 
-      document.querySelector('#couch-img-2').src = "images/003-sofa-2.png";
-      document.querySelector('#couch-img-2').alt = "Sofá vermelho";
-      document.querySelector('#couch-img-2').title = "Sofá vermelho";
+    document.querySelector('#couch-rotable-2').removeAttribute("class")
+    document.querySelector('#couch-rotable-2').classList.add("couch-rotable-2", "rotate-0");
 
-      // document.querySelector('#couch-img-3').src = "images/001-sofa.png";
-      // document.querySelector('#couch-img-3').alt = "Sofá azul";
-      // document.querySelector('#couch-img-3').title = "Sofá azul";
-    }, 500);
+    document.querySelector('#couch-rotable-1').classList.remove("rotate-0")
+    document.querySelector('#couch-rotable-1').classList.add("rotate-175")
+    document.querySelector('#couch-rotable-3').classList.remove("rotate-0")
+    document.querySelector('#couch-rotable-3').classList.add("rotate-175")
+
+    document.querySelector('#main-bg').removeAttribute("class")
+
+    document.querySelector('#main-bg').removeAttribute("class")
+    document.querySelector('#main-bg').classList.add("main-bg", "rotate-135");
     
     //troca os 3 sofás para amarelo
     document.querySelector('#couch-1').src = "images/002-sofa-1.png";
@@ -102,44 +62,27 @@ function changeSelected(e) {
     document.querySelector('.blue-couch').classList.remove("active");
     document.querySelector('.yellow-couch').classList.add("active");
     document.querySelector('.red-couch').classList.remove("active");
+
+    setTimeout(() => {  document.querySelector('#left-object-rotable-container').classList.remove("blur-container"); }, 1500);
   }
   if (e.target.alt === "Sofá Vermelho" || e.target.id === "Sofá Vermelho") {
-    // animações
-    if(document.querySelector('#divider').classList.value.includes("blue")) {
-      //de azul para vermelho
-      document.querySelector('#left-container').removeAttribute("class")
-      document.querySelector('#left-container').classList.add("left-container", "rotate-container");
-
-      document.querySelector('#main-bg').removeAttribute("class")
-      document.querySelector('#main-bg').classList.add("main-bg", "blue-to-red");
-
-      setTimeout(() => {  document.querySelector('#left-container').classList.remove("rotate-container"); }, 1501);
-    }
-    if(document.querySelector('#divider').classList.value.includes("yellow")) {
-      //de azul para amarelo
-      document.querySelector('#left-container').removeAttribute("class")
-      document.querySelector('#left-container').classList.add("left-container", "rotate-container");
-
-      document.querySelector('#main-bg').removeAttribute("class")
-      document.querySelector('#main-bg').classList.add("main-bg", "yellow-to-red");
-
-      setTimeout(() => {  document.querySelector('#left-container').classList.remove("rotate-container"); }, 1501);
-    }
-
     //troca o sofá principal para vermelho
-    setTimeout(() => {
-      document.querySelector('#couch-img').src = "images/003-sofa-2.png";
-      document.querySelector('#couch-img').alt = "Sofá vermelho";
-      document.querySelector('#couch-img').title = "Sofá vermelho";
+    document.querySelector('#left-object-rotable-container').removeAttribute("class")
+    document.querySelector('#left-object-rotable-container').classList.add("left-object-rotable-container", "blur-container");
 
-      document.querySelector('#couch-img-2').src = "images/001-sofa.png";
-      document.querySelector('#couch-img-2').alt = "Sofá azul";
-      document.querySelector('#couch-img-2').title = "Sofá azul";
+    document.querySelector('#couch-rotable-3').removeAttribute("class")
+    document.querySelector('#couch-rotable-3').classList.add("couch-rotable-3", "rotate-0");
 
-      // document.querySelector('#couch-img-3').src = "images/002-sofa-1.png";
-      // document.querySelector('#couch-img-3').alt = "Sofá amarelo";
-      // document.querySelector('#couch-img-3').title = "Sofá amarelo";
-    }, 500);
+    document.querySelector('#couch-rotable-1').classList.remove("rotate-0")
+    document.querySelector('#couch-rotable-1').classList.add("rotate-175")
+    document.querySelector('#couch-rotable-2').classList.remove("rotate-0")
+    document.querySelector('#couch-rotable-2').classList.add("rotate-175")
+
+    document.querySelector('#main-bg').removeAttribute("class")
+    document.querySelector('#main-bg').classList.add("main-bg", "rotate-15");
+
+    document.querySelector('#main-bg').removeAttribute("class")
+    document.querySelector('#main-bg').classList.add("main-bg", "rotate-255");
 
     //troca os 3 sofás para vermelho
     document.querySelector('#couch-1').src = "images/003-sofa-2.png";
@@ -154,5 +97,7 @@ function changeSelected(e) {
     document.querySelector('.blue-couch').classList.remove("active");
     document.querySelector('.yellow-couch').classList.remove("active");
     document.querySelector('.red-couch').classList.add("active");
+
+    setTimeout(() => {  document.querySelector('#left-object-rotable-container').classList.remove("blur-container"); }, 1500);
   }
 }
